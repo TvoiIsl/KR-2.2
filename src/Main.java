b4377e496dcb3efb075772b69251b17c91bb3866
Islam <islam.bedretdinov@yandex.ru>
2023-03-02 15:45:21 +0300

    Задача на функциональное программирование

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите текст:");
+        String s = scanner.nextLine();
+        int quantity = 0;
+        if (s.length() != 0) {
+            quantity++;
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == ' ') {
+                    quantity++;
+                }
+            }
+        }
+        System.out.println("Количество слов в тексте равно: " + quantity);
+        String[] sArray = s.split(" ");
+        Map<String, Integer> repetition = new HashMap<>();
+        for (String d : sArray) {
+            if (repetition.containsKey(d)) {
+                repetition.put(d, repetition.get(d) + 1);
+            } else {
+                repetition.put(d, 1);
+            }
+        }
+        System.out.println("ТОП 10:");
+        int t = 0;
+        for (Map.Entry<String, Integer> a : repetition.entrySet()) {
+            System.out.println(a.getKey() + " - " + a.getValue());
+            t = a.getValue();
+        }
     }
 }
